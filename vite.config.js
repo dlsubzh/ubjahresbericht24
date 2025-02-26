70797cfc6836e8bc157b670d0b163b293f43921f
dlsubzh <joshua.binswanger@gmail.com>
2025-02-26 12:18:46 +0100

    changed base to /ubjahresbericht24/

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 // vite.config.js
 export default {
     // config options
-    base: 'ubjahresbericht24', // Replace <REPO> with your repository name
+    base: '/ubjahresbericht24/', // Replace <REPO> with your repository name
   }
